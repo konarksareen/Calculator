f74a4a2e609ebebd69be00d1d49a89e3c6928a14
konark sareen <43315655+konarksareen@users.noreply.github.com>
2019-12-13 01:06:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+		
+		function display(val) 
+		{ 
+			document.getElementById("result").value+=val 
+		} 
+		
+		
+		function solve() 
+		{ 
+			let x = document.getElementById("result").value 
+            let y= eval(x);
+            document.getElementById("result").value = y ;
+		} 
+		
+		
+		function clear() 
+		{ 
+			document.getElementById("result").value = "" ;
+		} 
